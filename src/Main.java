945c61b2babd952d3b59445c2fc2e381c90a336b
Людок <skobeleva.ludmila@yandex.ru>
2022-11-19 12:48:19 +0300

    первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        // задание 1.
+        int bissextileYear = 1;
+        int a[] =[1544];
+        a[0] = 1;
+        a[1] = 2;
 
-        System.out.println("Hello world!");
     }
+
+    public static void bissextileYear(int[] a) {
+        if (a % 4 = 0 || a % 100 != 0) ;
+        System.out.println(a + " Год високосный ");
+        {
+        }else{
+            System.out.println(a + " Год не високосный ");
+
+        }
+    }
+
+
+}
+
+
 }
